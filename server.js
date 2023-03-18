52c2b24cf4d457e991186215e7616336a312071a
Jérémy Buget <jeremy.buget@macbook-pro-m2.home>
2023-03-18 20:40:48 +0100

    Add route to generate CPU load

--- server.js
+++ server.js
@@ -8,4 +8,17 @@ server.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
+server.get('/cpu', async (request, reply) => {
+  const items = [...Array(100000).keys()];
+  const nbLoops = 10000;
+  const result = items.map((item, index) => {
+    for (let step = 0; step < nbLoops; step++) {
+      // DO SOMETHING…
+    }
+    const value = Math.floor(Math.random() * nbLoops);
+    return { index, value };
+  });
+  return result;
+})
+
 export default server;
